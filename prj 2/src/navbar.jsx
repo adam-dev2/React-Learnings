a27d1b1cbdf6d433ebf156f88ea069af4342f32d
adam-dev2 <shaikadam252@gmail.com>
2024-10-26 05:28:01 +0530

    navbar update

--- prj 2/src/navbar.jsx
+++ prj 2/src/navbar.jsx
@@ -1,7 +1,21 @@
+import { Button } from '@mui/material';
 function Navbar() {
     return (
         <>
-            <h1>Navbar</h1>
+            <div style={{display:'flex', justifyContent:'space-between'}}>
+                <div>
+                    <img src="prj 2/src/images/Nike-Logo.png" alt="nikelogo" />
+                </div>
+                <div>
+                    <Button>MENU</Button>
+                    <Button>LOCATION</Button>
+                    <Button>ABOUT</Button>
+                    <Button>CONTACT</Button>
+                </div>
+                <div style={{backgroundColor:'red'}}>
+                    <Button>Login</Button>
+                </div>
+            </div>
         </>
     )
 }
